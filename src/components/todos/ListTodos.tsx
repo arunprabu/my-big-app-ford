eafedf2856fea61d51d65136a27d30cbb24b6d76
Arun Vijayarengan <arunprabu.v@hotmail.com>
2024-09-03 10:29:34 +0530

    updated

--- src/components/todos/ListTodos.tsx
+++ src/components/todos/ListTodos.tsx
@@ -1,16 +1,34 @@
-import { Box, List, ListItem, ListItemText } from '@mui/material'
+import { Box, List, ListItem, ListItemText } from "@mui/material";
+import { useTodosStore } from "../../store/useTodoStore";
+import { useEffect } from "react";
 
 const ListTodos = () => {
+  const { getTodos, todoList, isLoading, isError } = useTodosStore();
+  console.log(todoList);
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (isError) {
+    return <p>Error loading todos.</p>;
+  }
+
   return (
     <Box>
       <List dense={true}>
-        
-          <ListItem>
-            <ListItemText primary="my Todo 1" />
+        { todoList.map((todo) => (
+          <ListItem key={todo.id}>
+            <ListItemText primary={todo.title} />
           </ListItem>
+        ))}
       </List>
     </Box>
   );
-}
+};
 
-export default ListTodos
+export default ListTodos;
